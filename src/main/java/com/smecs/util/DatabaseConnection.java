041e519b05e3df5f67b9e2367a1d5e98c4d47816
wodoame <bmwodoame@gmail.com>
2026-01-12 07:02:36 +0000

    docs: add architecture document

--- src/main/java/com/smecs/util/DatabaseConnection.java
+++ src/main/java/com/smecs/util/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/smecs";
     private static final String USER = "postgres"; // Placeholder: Modify as per environment
-    private static final String PASSWORD = "password"; // Placeholder: Modify as per environment
+    private static final String PASSWORD = "Bernardxx2003"; // TODO: use environment variables
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
