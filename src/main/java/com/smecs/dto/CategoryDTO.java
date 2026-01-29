41ec12d830ce6d6ffb1a77764d064e76dfd46929
Wodoame <bmwodoame@gmail.com>
2026-01-29 16:02:45 +0000

    refactor: replace getters and setters with lombok annotations

--- src/main/java/com/smecs/dto/CategoryDTO.java
+++ src/main/java/com/smecs/dto/CategoryDTO.java
@@ -1,5 +1,10 @@
 package com.smecs.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class CategoryDTO {
     private Integer categoryId;
     private String categoryName;
@@ -11,10 +16,4 @@ public class CategoryDTO {
         this.categoryName = categoryName;
         this.description = description;
     }
-    public Integer getCategoryId() { return categoryId; }
-    public void setCategoryId(Integer categoryId) { this.categoryId = categoryId; }
-    public String getCategoryName() { return categoryName; }
-    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
 }
--- src/main/java/com/smecs/dto/ProductDTO.java
+++ src/main/java/com/smecs/dto/ProductDTO.java
@@ -1,20 +1,14 @@
 package com.smecs.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductDTO {
     private Long id;
     private String name;
     private String description;
     private Double price;
     private Long categoryId;
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public Double getPrice() { return price; }
-    public void setPrice(Double price) { this.price = price; }
-    public Long getCategoryId() { return categoryId; }
-    public void setCategoryId(Long categoryId) { this.categoryId = categoryId; }
 }
