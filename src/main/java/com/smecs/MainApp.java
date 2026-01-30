fca328fccaa801d534114d284686db6e5806899c
Wodoame <bmwodoame@gmail.com>
2026-01-30 11:43:15 +0000

    refactor: remove legacy controllers

--- src/main/java/com/smecs/MainApp.java
+++
@@ -1,1 +0,0 @@
-// File removed: This class was for JavaFX Application and is obsolete after migration to Spring Boot.
--- src/main/java/com/smecs/controller/ProductFormController.java
+++
@@ -1,4 +0,0 @@
-package com.smecs.controller;
-
-// JavaFX code removed due to migration to Spring Boot. This controller was UI-only.
-// If backend logic is needed, implement as a Spring @RestController or @Controller.
