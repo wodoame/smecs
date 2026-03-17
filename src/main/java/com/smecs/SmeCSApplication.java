0297f3126dcb2c0794dd5312c60a0e491a605ca5
Wodoame <bmwodoame@gmail.com>
2026-03-17 12:24:25 +0000

    refactor: remove @EnableAsync annotation and allow async configuration to take over

--- src/main/java/com/smecs/SmeCSApplication.java
+++ src/main/java/com/smecs/SmeCSApplication.java
@@ -3,24 +3,15 @@ package com.smecs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
+// async configuration is controlled via AsyncConfig
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
-@EnableAsync
 public class SmeCSApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmeCSApplication.class, args);
-    }
-
-    @RestController
-    static class HelloController {
-        @GetMapping("/")
-        public String hello() {
-            return "Hello, World!";
-        }
     }
 }
